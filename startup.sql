a2ad4f073bf8e22022a91ca752998c3c9d52082a
RicksLWilliams <rlwilliamsix@yahoo.com>
2020-05-19 09:01:38 -0500

    changes

--- startup.sql
+++ startup.sql
@@ -1,4 +1,7 @@
-  
+--git remote add origin https://github.com/RicksLWilliams/store.git
+--git push -u origin master
+
+
 CREATE TABLE shoes (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(80) NOT NULL,
